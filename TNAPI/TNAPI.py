ac1f135a99f00cb70032bfe8d3554da2849ba831
Leo Wu-Gomez <leojwu18@gmail.com>
2021-03-21 20:35:55 -0600

    Linux abspath

--- TNAPI/TNAPI.py
+++ TNAPI/TNAPI.py
@@ -3,6 +3,7 @@ from TNAPI.login import login
 import requests
 from datetime import datetime
 import json
+from os.path import abspath
 
 MESSAGE_TYPE = 0
 MULTIMEDIAMESSAGE_TYPE = 1
@@ -13,7 +14,7 @@ RECEIVED_MESSAGE_TYPE = 1
 class Client():
     def __init__(self, email: str, password: str, name: str = ""):
         #Load SIDS
-        user_SID_filepath = "\\".join(__file__.split("\\")[:-1]) + "\\user_sids.json"
+        user_SID_filepath = "\\".join(abspath(__file__).split("\\")[:-1]) + "\\user_sids.json"
         user_SIDS_file = open(user_SID_filepath, mode="r+")
         user_SIDS = json.loads(user_SIDS_file.read())
 
